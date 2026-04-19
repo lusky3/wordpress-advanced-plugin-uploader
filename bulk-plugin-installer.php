1e524ed15fb153b006fa40cd5484a067fe30af54
lusky3 <lusky3@users.noreply.github.com>
2026-04-19 18:07:55 -0400

    chore: bump version to 1.0.2

--- bulk-plugin-installer.php
+++ bulk-plugin-installer.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Bulk Plugin Installer
  * Plugin URI:        https://github.com/lusky3/bulk-plugin-installer
  * Description:       Upload and install multiple WordPress plugin ZIP files in a single operation with preview, rollback, and profile support.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 5.8
  * Requires PHP:      8.2
  * Author:            Cody (lusky3) + Bulk Plugin Installer Contributors
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Plugin constants.
-define( 'BPI_VERSION', '1.0.1' );
+define( 'BPI_VERSION', '1.0.2' );
 define( 'BPI_PLUGIN_FILE', __FILE__ );
 define( 'BPI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BPI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
